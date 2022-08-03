312b6a386935b0ac6da643348eccc42b94197b54
Brian Denmark <testingarea479@gmail.com>
2022-08-03 14:59:45 -0700

    added pseudo code for Values Greater Than Second exercise

--- file.py
+++ file.py
@@ -31,3 +31,18 @@ def first_plus_length(list):
     return first + length
 
 print(first_plus_length([1,2,3,5,6,7,8,9,10,11,12,13]))
+
+# Values greater than second - accepts a list and creates a new list containing only the values from the original list
+# that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements,
+# have the function return False
+    # Example: values_greater_than_second([5,2,3,2,1,4]) should print 3 and return [5,3,4]
+    # Example: values_greater_than_second([3]) should return False
+
+# define function which uses list as param
+    # create a new list which will contain our return values
+    # create a variable to store the second value of the arg list which gets passed in
+    # check the length of incoming list to make sure it's greater than 2
+        # if list length is 2 or less, return False
+    # check each index of the list to see if it's greater than the 2nd value of the list
+        # if value is greater, add it to a new list
+    # if value is less, ignore that value
